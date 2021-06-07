e19cb58188c7f4d549181921e3cbac5f5bdd6dc3
Lionel Ritchie <lionelrtchieee@gmail.com>
2021-06-07 12:47:12 +0700

    added data anotation valdiator

---
+++ RDT_WEB_LL/Models/Form Models/AddQuestionFormModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RDT_WEB_LL.Models.Form_Models
+{
+    public class AddQuestionFormModel
+    {
+        [Required]
+        public string QuestionText { get; set; }
+
+        public string QuestionType { get; set; }
+
+
+        public List<PossibleAnswerFormModel> PossibleAnswers { get; set; }
+
+        public AddQuestionFormModel()
+        {
+            this.PossibleAnswers = new List<PossibleAnswerFormModel>();
+        }
+    }
+}
---
+++ RDT_WEB_LL/Models/Form Models/PossibleAnswerFormModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RDT_WEB_LL.Models.Form_Models
+{
+    public class PossibleAnswerFormModel
+    {
+        public string Value { get; set; }
+    }
+}
